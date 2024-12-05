a8373dd015a04e512f6024c42dc83e6d8db3606e
saemii-24 <hims2419@gmail.com>
2024-12-06 00:36:10 +0900

    Chore: firebase 세팅

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: './',
 });
 
 // Add any custom config to be passed to Jest
 const config: Config = {
-  coverageProvider: "v8",
-  testEnvironment: "jsdom",
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/src/app/components/$1",
+    '^@/components/(.*)$': '<rootDir>/src/app/components/$1',
   },
 };
 
---
+++ src/remote/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const {
+  NEXT_APP_API_KEY,
+  NEXT_APP_AUTH_DOMAIN,
+  NEXT_APP_PROJECT_ID,
+  NEXT_APP_STORAGE_BUCKET,
+  NEXT_APP_MESSAGING_SENDER_ID,
+  NEXT_APP_APP_ID,
+  NEXT_APP_MEASUREMENT_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: NEXT_APP_API_KEY,
+  authDomain: NEXT_APP_AUTH_DOMAIN,
+  projectId: NEXT_APP_PROJECT_ID,
+  storageBucket: NEXT_APP_STORAGE_BUCKET,
+  messagingSenderId: NEXT_APP_MESSAGING_SENDER_ID,
+  appId: NEXT_APP_APP_ID,
+  measurementId: NEXT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
