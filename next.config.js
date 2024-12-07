19692f1e329f3558acbaa9a8f8f4713639bfe944
saemii-24 <hims2419@gmail.com>
2024-12-08 01:19:01 +0900

    chore: msw 세팅

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/test',
+        destination: 'https://localhost:3000/test',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
-const page = () => {
-  async function app() {
-    const response = await fetch('https://example.com/user');
-    const user = await response.json();
-    console.log(user);
-  }
-
-  app();
-  return <div>page</div>;
+type User = {
+  id: string; // UUID 형식
+  firstName: string; // 이름
+  lastName: string; // 성
 };
 
-export default page;
+async function getUser(): Promise<User> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/test`, {
+    cache: 'no-store',
+  }); // 캐싱 방지
+  if (!response.ok) {
+    throw new Error('Failed to fetch user');
+  }
+  return response.json();
+}
+
+const Page = async () => {
+  const user = await getUser();
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      <p>ID: {user.id}</p>
+      <p>First Name: {user.firstName}</p>
+      <p>Last Name: {user.lastName}</p>
+    </div>
+  );
+};
+
+export default Page;
