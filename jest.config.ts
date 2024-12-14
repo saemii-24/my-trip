f97f3575fde4df41c22bd8e0c2060a2a1642fa82
saemii-24 <hims2419@gmail.com>
2024-12-14 21:38:02 +0900

    chore: jest zustand 세팅

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const config: Config = {
+  preset: 'ts-jest',
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
