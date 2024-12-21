d923d9f23b44c51d7b0635e7218b8216d49515d9
saemii-24 <hims2419@gmail.com>
2024-12-22 01:40:36 +0900

    feat: add gallery reload button

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,22 +5,31 @@ import React, { useState, useEffect } from "react";
 const rendomImageUrl = (numImages: number) => {
   const urls = [];
   for (let i = 0; i < numImages; i++) {
-    // 랜덤 이미지 사이트를 활용한다.
-    urls.push(`https://picsum.photos/600/500?random=${3*i}`);
+    urls.push(`https://picsum.photos/600/500?random=${3 * i}`);
   }
   return urls;
 };
 
-const Gallery = () => {
+interface GalleryProps {
+  numImages?: number;
+  //스토리북 사용자가 랟넘 이미지를 불러올 수 있도록 조치한다.
+  onReloadImages?: () => void; 
+}
+
+const Gallery: React.FC<GalleryProps> = ({ numImages = 12, onReloadImages }) => {
   const [imageUrls, setImageUrls] = useState<string[]>([]);
 
-  useEffect(() => {
-    const urls = rendomImageUrl(12);
+  const loadImages = () => {
+    const urls = rendomImageUrl(numImages);
     setImageUrls(urls);
+    if (onReloadImages) onReloadImages(); 
+  };
+
+  useEffect(() => {
+    loadImages();
   }, []);
 
   return (
-
     <div className="p-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {imageUrls.map((url, idx) => (
@@ -37,7 +46,8 @@ const Gallery = () => {
           </div>
         ))}
       </div>
-    </div>);
+    </div>
+  );
 };
 
 export default Gallery;
