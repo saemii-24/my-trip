8cc4fde0c5b48c47716f736e85f0ed1040fffbd1
saemii-24 <hims2419@gmail.com>
2025-06-22 17:40:17 +0900

    🚀 feat: 설문지 좌측 안내 사항 배치
    설문지 좌측의 안내 사항 퍼블리싱
    
    ref: #38

--- src/components/questionnaire/Questionnaire.tsx
+++ src/components/questionnaire/Questionnaire.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Questionnaire = () => {
   return (
-    <div className='bg-red-300 min-h-screen flex-1 p-10'>
+    <div className='bg-red-300 min-h-screen flex-1 p-12'>
       <div className='bg-white size-full rounded-3xl'></div>
     </div>
   );
--- src/components/questionnaire/StepNotice.tsx
+++ src/components/questionnaire/StepNotice.tsx
@@ -1,7 +1,83 @@
+'use client';
+import { questionnaireItems } from '@constant/questionnaireItems';
+import { useFirebaseAuth } from '@hooks/useFirebaseAuth';
+import { cn } from '@utils/cn';
 import React from 'react';
 
 const StepNotice = () => {
-  return <div className='w-[300px] bg-white'>StepNotice</div>;
+  let currentStep = 1;
+  const userValue = '서울';
+  const { user, loading, isLoggedIn } = useFirebaseAuth();
+  return (
+    <div className='min-h-[100vh-60px] pt-[60px] w-[450px] bg-white'>
+      <div className='p-5 '>
+        <div className='mt-6'>
+          <h1 className='text-3xl font-semibold'>
+            {user?.displayName || '여행자'}님의
+            <br /> 여행 취향을 알려주세요
+          </h1>
+          <h2 className='mt-1'>
+            {user?.displayName || '여행자'}님만을 위한 여행을 준비하고 있어요.
+            <br />
+            어떤 순간을 기대하고 계신가요?
+          </h2>
+        </div>
+        <div className='mt-8'>
+          {questionnaireItems.map((questionnaireItem, index) => {
+            return (
+              <StepEachContent
+                key={questionnaireItem.id}
+                questionnaireItem={questionnaireItem}
+                isAnswered={currentStep >= index}
+                userValue={userValue}
+                questionNumber={index}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StepNotice;
+
+interface StepEachContentProps {
+  id: string;
+  title: string;
+  waitAnswer: string;
+  selectedNotice: string;
+}
+
+const StepEachContent = ({
+  questionnaireItem,
+  isAnswered,
+  userValue,
+  questionNumber,
+}: {
+  questionnaireItem: StepEachContentProps;
+  isAnswered: boolean;
+  userValue: string;
+  questionNumber: number;
+}) => {
+  const { title, waitAnswer, selectedNotice } = questionnaireItem;
+  const { user, loading, isLoggedIn } = useFirebaseAuth();
+  return (
+    <div className='flex gap-3'>
+      <div className='flex flex-col items-center'>
+        <div className='size-6 shrink-0 bg-lime-400 rounded-full'></div>
+        <div
+          className={cn('w-[1px] h-[72px] bg-lime-400 items-stretch', {
+            hidden: questionNumber == 3,
+          })}
+        ></div>
+      </div>
+      <div>
+        <h2 className='text-xl font-bold text-gray-900'>{title}</h2>
+        <h3 className='text-base text-gray-700'>
+          {isAnswered ? selectedNotice.replace('@@', userValue ?? '') : waitAnswer}
+        </h3>
+      </div>
+    </div>
+  );
+};
