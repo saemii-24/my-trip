133a55e64563a2a7d414a22e393ea6a1a0f43659
saemii-24 <hims2419@gmail.com>
2025-07-08 10:52:08 +0900

    🎨 style: hero section 디자인 변경
    ref: #10

--- src/components/public/Hero.tsx
+++ src/components/public/Hero.tsx
@@ -7,10 +7,13 @@ import BounceLoading from './BounceLoading';
 import { useGeminiGet } from '../../query/useGeminiGet';
 import heroPromptFunc from '../../prompt/hero';
 import HeroInfo from './HeroInfo';
+import { ChevronRight } from 'lucide-react';
 
 const Hero = () => {
   const { country } = countryObj;
   const { unsplashData, unsplashIsLoading } = useUnsplashGet(country);
+
+  console.log(unsplashData);
 
   //Gemini Prompt
   const [prompt, setPrompt] = useState('');
@@ -32,24 +35,42 @@ const Hero = () => {
   }
 
   return (
-    <div className=' w-[calc(100vw-32px)] h-[calc(100vh-120px)] rounded-2xl overflow-hidden mx-auto relative'>
-      <div className='size-full relative'>
-        {/* 이미지 배경 */}
-        {unsplashData?.urls && (
-          <Image
-            src={unsplashData?.urls.full}
-            alt={unsplashData?.alt_description}
-            fill
-            style={{ objectFit: 'cover' }}
-            priority
-          />
-        )}
+    <div className=' w-[calc(100vw-32px)] h-[calc(100vh-120px)] rounded-[60px] overflow-hidden mx-auto relative'>
+      <div className='size-full relative '>
+        <Image
+          src={unsplashData?.urls.full || ''}
+          alt={unsplashData?.alt_description || ''}
+          fill
+          style={{ objectFit: 'cover' }}
+          priority
+        />
 
         {/* 블랙 그라데이션 오버레이 */}
-        <div className='absolute inset-0 bg-gradient-to-b from-black/70 to-black/0 z-10'></div>
+        <div className='absolute  inset-0 bg-gradient-to-b to-black/70 from-black/0 z-10'></div>
 
         {/* 콘텐츠 영역 */}
-        <div className='absolute inset-0 z-20 flex flex-col items-center justify-center text-center text-white px-4'>
+        <div className='absolute bottom-20 left-20 text-white z-[10] '>
+          <h1 className='text-4xl md:text-6xl font-bold mb-4 max-w-[55%] !leading-[130%] break-keep'>
+            {geminiData?.title}
+          </h1>
+          <p className='text-lg md:text-xl mb-6  max-w-[60%] break-keep'>
+            {geminiData?.content}
+          </p>
+          <div className='flex items-center gap-3'>
+            <button className='bg-green-600 text-white  font-medium text-lg px-8 py-3 rounded-full hover:bg-green-700 transition'>
+              See more
+            </button>
+            <button className=' text-white group font-medium text-lg px-8 py-3 rounded-full transition flex items-center gap-5'>
+              <span className='group-hover:underline'>Make Plan</span>
+              <ChevronRight strokeWidth={2} className='text-white size-5' />
+            </button>
+          </div>
+        </div>
+        <HeroInfo
+          className='absolute z-[10] bottom-20 right-20'
+          unsplashData={unsplashData}
+        />
+        {/* <div className='absolute inset-0 z-20 flex flex-col items-start justify-center text-start text-white px-4'>
           <h1 className='text-4xl md:text-6xl font-bold mb-4'>{geminiData?.title}</h1>
           <p className='text-lg md:text-xl mb-6 max-w-2xl'>{geminiData?.content}</p>
           <button className='bg-white text-black font-medium px-6 py-3 rounded-full hover:bg-gray-100 transition'>
@@ -59,7 +80,7 @@ const Hero = () => {
         <HeroInfo
           className='absolute z-[100] bottom-10 right-10'
           unsplashData={unsplashData}
-        />
+        /> */}
       </div>
     </div>
   );
