ab90cb4a1e1c825e3b2a763ada6944da44ba658a
saemii-24 <hims2419@gmail.com>
2025-07-15 18:08:39 +0900

    🚀 feat: auto Complete 수정
    ref: #28

--- src/components/public/Header.tsx
+++ src/components/public/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const { isLoggedIn } = useFirebaseAuth();
 
   return (
-    <header className='h-[80px] bg-white z-[100] flex items-center justify-between px-5  fixed top-0 right-0 left-0 '>
+    <header className='h-[80px]  z-[100] flex items-center justify-between px-5  fixed top-0 right-0 left-0 '>
       <Link href='/'>
         <div className='flex items-center gap-1'>
           <Plane strokeWidth={2} className='text-green-500 size-8' />
@@ -19,7 +19,7 @@ const Header = () => {
       </Link>
       <div className='items-center flex gap-2'>
         <Link
-          href='/login'
+          href='/selectCountry'
           className='bg-green-500 bg-gradient-to-r to-green-500 from-green-600 text-white px-8 rounded-full py-2 font-bold text-lg'
         >
           Travel Country
